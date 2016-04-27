332ec6c43e17d05cf464698ee55a2d43787b86ed
sagalasan <sagalasan@users.noreply.github.com>
2016-04-26 21:01:00 -0600

    Add mysql function

--- routes/lib/mysqlconnection.js
+++ routes/lib/mysqlconnection.js
@@ -1,6 +1,10 @@
-require('mysql');
-
-function newMysqlConnection()
-{
-  return new M
+function newMysqlConnection(mysql) {
+  return mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'imuser',
+    password: 'impass',
+    database: 'sagalasan'
+  });
 }
+
+module.exports = newMysqlConnection;
