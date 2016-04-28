7ed853820adeb322382731514e2e81803a2ac7c6
sagalasan <sagalasan@users.noreply.github.com>
2016-04-28 11:25:54 -0600

    Add get single queries

--- routes/lib/queries.js
+++ routes/lib/queries.js
@@ -7,3 +7,13 @@ exports.getEmployeesQuery = function()
 {
   return "SELECT * FROM cs464_employees;";
 };
+
+exports.getSingleClient = function(client_id)
+{
+  return "SELECT * FROM cs464_clients WHERE client_id = " + client_id;
+};
+
+exports.getSingleEmployee = function(employee_id)
+{
+  return "SELECT * FROM cs464_employees WHERE employee_id = " + employee_id;
+};
